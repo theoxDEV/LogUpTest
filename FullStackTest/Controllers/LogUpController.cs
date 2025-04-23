324e05a619b5d82c829efb980fe8ecb4fa314166
Matheus Nascimento <56891192+theoxDEV@users.noreply.github.com>
2025-04-23 19:53:38 -0300

    Add project files.

---
+++ FullStackTest/Controllers/LogUpController.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FullStackTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogUpController : ControllerBase
+    {
+
+        public LogUpController()
+        {
+            
+        }
+
+        [HttpGet(Name = "Get")]
+        public List<LogUp> Get()
+        {
+            var logUp = new Faker<LogUp>()
+
+            .RuleFor(p => p.Id, p => Guid.NewGuid())
+            .RuleFor(p => p.Title, p => p.Company.CompanyName())
+            .RuleFor(p => p.CreatedOn, p => p.Date.Past())
+            .RuleFor(p => p.Description, p => p.Lorem.Paragraphs())
+            .RuleFor(p => p.Comments, p => p.Lorem.Paragraphs());
+
+            var logUpList = new List<LogUp>();
+
+            for (int i=0; i<10; i++)
+            {
+                logUpList.Add(logUp.Generate());
+            }
+
+            return logUpList;
+        }
+    }
+}
---
+++ FullStackTest/LogUp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace FullStackTest
+{
+    public class LogUp
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string Comments { get; set; }
+    }
+}
